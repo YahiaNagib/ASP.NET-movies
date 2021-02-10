4fe49d3d55ca79c30922531590c0dcb16b1d6f57
YahiaNagib <yahianagib123@gmail.com>
2021-02-10 17:10:02 +0200

    initial commit

---
+++ TheApp/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheApp.Models;
+
+namespace TheApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Yahia" };
+            return View(movie);
+        }
+    }
+}
